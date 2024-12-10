b616f94c11a8037af9dd97ff7a6a3580be7334df
Tossoukpe Kossi Serge <kossi-serge.tossoukpe@univ-rennes.fr>
2024-12-10 16:04:50 +0100

    User and LogInfo first attempt done

--- User.java
+++ User.java
@@ -1,3 +1,33 @@
-public class User{
+public class User {
+    
+    boolean isAdmin=false;
+    String Username;
+    int id;
+    String mdp;
 
-}
+    public User(int identifier, String pwd, boolean isAd, String nom){
+        identifier=id;
+        pwd=mdp;
+        isAd=isAdmin;
+        nom=Username;
+    }
+    
+
+    public void Update(int new_id, String new_mdp, String new_Username, boolean status) {
+        if (id!=new_id){
+            id=new_id;
+        } 
+        if (mdp!=new_mdp){
+            id=new_id;
+        } 
+        if (isAdmin!=status){
+            isAdmin = true;
+        } 
+        if (Username!=new_Username){
+            Username=new_Username;
+        }
+      
+    }
+ 
+
+};
