1e97017c9d47ab032974b28b5e913e7a052d0976
Tossoukpe Kossi Serge <kossi-serge.tossoukpe@univ-rennes.fr>
2025-01-19 11:11:51 +0100

    updated LogInfo.java

--- LogInfo.java
+++ LogInfo.java
@@ -1,40 +1,22 @@
-import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
+public class LogInfo {
+    private ArrayList<String> logs;
 
-
-
-public class LogInfo{
-    String Username;
-    String mdp;
-    String Accountname;
-    byte[] cy_Username;
-    byte[] cy_mdp;
-    
-    public LogInfo(String pwd, String nom, byte[] cle) {
-        this.mdp = pwd;
-        this.Username = nom;
-
+    public LogInfo() {
+        this.logs = new ArrayList<>();
     }
 
-
-    public static IvParameterSpec GenerateIv() {
-        byte[] initializationVector = new byte[16];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(initializationVector);
-        return new IvParameterSpec(initializationVector);
+    public void logAction(String action) {
+        String timestamp = LocalDateTime.now().toString();
+        logs.add(timestamp + " - " + action);
     }
 
-
-    public void Cypher(SecretKey secretKey, IvParameterSpec iv) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES/CFB8/NoPadding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
-        cy_Username=cipher.doFinal(Username.getBytes(StandardCharsets.UTF_8));
-        cy_mdp=cipher.doFinal(mdp.getBytes(StandardCharsets.UTF_8));
+    public void displayLogs() {
+        System.out.println("Journal des actions :");
+        for (String log : logs) {
+            System.out.println(log);
+        }
     }
-
-   
-};
+}
