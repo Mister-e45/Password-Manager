f980aacb93c0572733c76ca7ba349a69cd500ed8
Tossoukpe Kossi Serge <kossi-serge.tossoukpe@univ-rennes.fr>
2025-01-28 20:49:32 +0100

    Deleted useless methods in UserInput

--- UserInput.java
+++ UserInput.java
@@ -5,18 +5,16 @@ public class UserInput {
 
     // Constructeur
     public UserInput() {
-        this.scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
     }
 
     public String getStringInput(String prompt) {
         System.out.print(prompt);
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public int getIntInput(String prompt) {
         System.out.print(prompt);
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
 }
